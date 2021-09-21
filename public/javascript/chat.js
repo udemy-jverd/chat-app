680628b7e38f06b80ac46759c31b708c53b340aa
Joffrey <jverd@protonmail.com>
2021-09-21 17:27:11 +0200

    feature(socketio): implement first websocket

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,1 +1,26 @@
-io();
+const socket = io();
+
+socket.on('message', (message) => {
+    console.log(message);
+});
+
+const chatForm = document.getElementById('chat');
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = e.target.elements.messageContent.value;
+    socket.emit('newMessage', message, (callbackMessage) => {
+        console.log(callbackMessage);
+    });
+});
+
+document.getElementById('shareLocation').addEventListener('click', (e) => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser!');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        socket.emit('shareLocation', { latitude, longitude }, (callbackMessage) => {
+            console.log(callbackMessage);
+        });
+    });
+});
--- src/index.js
+++ src/index.js
@@ -12,8 +12,28 @@ const publicDirectory = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectory));
 
-io.on('connection', () => {
-    console.log('New web socket onnection!');
+// socket.emit call the event only for the client who ask
+// socket.broadcast.emit call the event for every connected client exect the current one
+// io.emit call the event for every connected client
+
+io.on('connection', (socket) => {
+
+    socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined the chat room!');
+
+    socket.on('newMessage', (message, callback) => {
+        io.emit('message', message);
+        callback('Message delivered!');
+    });
+    socket.on('shareLocation', (coords, callback) => {
+        const { latitude, longitude } = coords;
+        io.emit('message', `https://google.com/maps?q=${latitude},${longitude}`);
+        callback('Location shared!');
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat room!');
+    });
 });
 
 server.listen(port, () => {
